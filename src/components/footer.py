40fbe591c5cbf3d674ececb8dddf84029c255a72
Madhav Chotaliya <madhavchotaliya319@gmail.com>
2026-06-29 21:40:11 +0530

    Add main application logic and UI components for SNAPCLASS
    - Implement main function to handle user login types (teacher, student).
    - Create home screen with options for students and teachers.
    - Add header and footer components for consistent layout.
    - Define styles for background and layout using Streamlit.
    - Introduce separate screens for student and teacher functionalities.

---
+++ src/components/footer.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def footer_home():
+    
+    
+    
+    st.markdown(f"""
+        <div style="margin-top:rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by mdx19 </p>
+        </div>       
+                
+                """, unsafe_allow_html=True)
